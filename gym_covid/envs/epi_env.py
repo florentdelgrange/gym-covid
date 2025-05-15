234ad8c74bbb295f3a577f359cf4f6654b604134
florentdelgrange <florent.delgrange@gmail.com>
2025-05-15 09:18:24 +0200

    changed the current susceptible (at day 7) to the susceptible at day 1 (called here S_0)

--- gym_covid/envs/epi_env.py
+++ gym_covid/envs/epi_env.py
@@ -79,6 +79,7 @@ class EpiEnv(gym.Env):
 
         # simulate for a whole week, sum the daily rewards
         r_ari = r_arh = r_sr = 0.
+        S_0 = -1.
         state_n = np.empty((self.days_per_timestep,) + self.observation_space.shape)
         event_n = np.zeros((self.days_per_timestep, 1), dtype=bool)
         for day in range(self.days_per_timestep):
@@ -105,7 +106,8 @@ class EpiEnv(gym.Env):
             S_s = s[self.model.S]
             S_s_n = s_n[self.model.S]
             r_ari += -(np.sum(S_s) - np.sum(S_s_n))
-            current_susceptible = np.sum(S_s)
+            if S_0 == -1:
+                S_0 = np.sum(S_s)
             # attack rate hospitalization
             I_hosp_new_s_n = s_n[self.model.I_hosp_new] + s_n[self.model.I_icu_new]
             r_arh += -np.sum(I_hosp_new_s_n)
@@ -131,4 +133,4 @@ class EpiEnv(gym.Env):
         # next-state , reward, terminal?, info
         # provide action as proxy for current SCM, impacts progression of epidemic
         return ((state_n, event_n, action.copy()), np.array([r_ari, r_arh, r_sr_w, r_sr_s, r_sr_l]), False,
-                {"current_susceptible": current_susceptible})
+                {"S_0": S_0})
